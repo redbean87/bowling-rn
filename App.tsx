00d87086d08d4871c9efe15ae4dc233a54619cd6
Cortez Ashley <cortezashley87@gmail.com>
2023-03-30 22:43:02 -0500

    lint configuration

--- App.tsx
+++ App.tsx
@@ -1,9 +1,8 @@
 import 'react-native-gesture-handler';
-import { SafeAreaView, StyleSheet } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import Game from './src/components/Game';
 import Home from './src/components/Home';
@@ -20,7 +19,7 @@ const queryClient = new QueryClient({
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <SafeAreaView style={styles.container}>
@@ -39,10 +38,12 @@ export default function App() {
       </SafeAreaView>
     </QueryClientProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
+export default App;
