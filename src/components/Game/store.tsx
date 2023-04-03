c02af3b74926d8174a9e525f747188c2b3e162a6
Cortez Ashley <cortezashley87@gmail.com>
2023-04-02 23:01:51 -0500

    use AsyncStorage from react native

--- src/components/Game/store.tsx
+++ src/components/Game/store.tsx
@@ -1,4 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 const MAX_FRAME_INDEX = 9;
 
@@ -14,48 +16,56 @@ interface GameStore {
   onNextPress: () => void;
 }
 
-export const useGameStore = create<GameStore>()((set, get) => {
-  return {
-    frameIndex: 0,
-    rollIndex: 0,
-    isStrikeBall: () => {
-      const { frameIndex, rollIndex } = get();
-      if (maxFrameCheck(frameIndex)) {
-        return true;
-      }
-      return rollIndex === 0;
+export const useGameStore = create<GameStore>()(
+  persist(
+    (set, get) => {
+      return {
+        frameIndex: 0,
+        rollIndex: 0,
+        isStrikeBall: () => {
+          const { frameIndex, rollIndex } = get();
+          if (maxFrameCheck(frameIndex)) {
+            return true;
+          }
+          return rollIndex === 0;
+        },
+        setFrameIndex: (value: number) => set(() => ({ frameIndex: value })),
+        onStrikePress: () =>
+          set((state) => {
+            const { frameIndex } = state;
+            if (maxFrameCheck(frameIndex)) {
+              return {};
+            }
+            return {
+              frameIndex: frameIndex + 1,
+            };
+          }),
+        onSparePress: () =>
+          set((state) => {
+            const { frameIndex } = state;
+            if (maxFrameCheck(frameIndex)) {
+              return {};
+            }
+            return {
+              frameIndex: frameIndex + 1,
+            };
+          }),
+        onNextPress: () =>
+          set((state) => {
+            const { frameIndex, rollIndex } = state;
+            if (maxFrameCheck(frameIndex)) {
+              return {};
+            }
+            if (rollIndex > 0) {
+              return { frameIndex: frameIndex + 1, rollIndex: 0 };
+            }
+            return { rollIndex: 1 };
+          }),
+      };
     },
-    setFrameIndex: (value: number) => set(() => ({ frameIndex: value })),
-    onStrikePress: () =>
-      set((state) => {
-        const { frameIndex } = state;
-        if (maxFrameCheck(frameIndex)) {
-          return {};
-        }
-        return {
-          frameIndex: frameIndex + 1,
-        };
-      }),
-    onSparePress: () =>
-      set((state) => {
-        const { frameIndex } = state;
-        if (maxFrameCheck(frameIndex)) {
-          return {};
-        }
-        return {
-          frameIndex: frameIndex + 1,
-        };
-      }),
-    onNextPress: () =>
-      set((state) => {
-        const { frameIndex, rollIndex } = state;
-        if (maxFrameCheck(frameIndex)) {
-          return {};
-        }
-        if (rollIndex > 0) {
-          return { frameIndex: frameIndex + 1, rollIndex: 0 };
-        }
-        return { rollIndex: 1 };
-      }),
-  };
-});
+    {
+      name: 'game-state',
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
